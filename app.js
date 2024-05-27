26d60a830ed11c5274ebb3f6c148610ee7fb818c
Atanu <atanum991@gmail.com>
2024-05-27 11:49:30 +0530

    update

--- app.js
+++ app.js
@@ -50,37 +50,38 @@ setTimeout(() => {
             let image = cart_elem.querySelector("#image").getAttribute("src");
             let cartdiv = document.createElement("div");
             cartdiv.innerHTML = `
-<div className="container" id="the_cart">
-    <img src="${image}" alt="">
-    <p>${title}</p>
-    <p>${price}</p>
-    <button class="del_cart" id="remove_cart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
-        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-    </svg></button>
-</div>
-`
+                <div className="container" id="the_cart">
+                <img src="${image}" alt="">
+                <p>${title}</p>
+                <p>${price}</p>
+                <button class="del_cart" id="remove_cart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                </svg></button>
+                </div>
+                `
             document.getElementById("cart_section").style.background = "none";
             document.getElementById("check_btn").style.display = "inline-block";
             document.getElementById("cart_elem").appendChild(cartdiv);
             document.getElementById("alerts").innerHTML = `
-<div class="alert alert-primary alert-dismissible fade show float-end" role="alert">
-    <strong>Cart Added!</strong>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-`
+                <div class="alert alert-primary alert-dismissible fade show float-end" role="alert">
+                <strong>Cart Added!</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                `
                 // =================cart remove=================
-            let del_btn = document.getElementsByClassName("del_cart");
-            let del_arr = Array.from(del_btn);
-            del_arr.forEach((del) => {
-                del.addEventListener("click", (e) => {
-                    document.getElementById("alerts").innerHTML = `
-<div class="alert alert-danger alert-dismissible fade show float-end" role="alert">
-    <strong>Cart Removed!</strong>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-</div>
-`
-                    let del_elem = e.target.parentNode.parentNode;
-                    del_elem.style.display = "none";
+        let del_btn = document.getElementsByClassName("del_cart");
+        let del_arr = Array.from(del_btn);
+        del_arr.forEach((del) => {
+            del.addEventListener("click", (e) => {
+                document.getElementById("alerts").innerHTML = `
+                <div class="alert alert-danger alert-dismissible fade show float-end" role="alert">
+                <strong>Cart Removed!</strong>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                `
+                let del_elem = e.target.parentNode.parentNode;
+                del_elem.style.display = "none";
+
                 });
             });
         })
